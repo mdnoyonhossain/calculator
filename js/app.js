22d7b1b63222d82265146823054a51af68293ee6
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-08-19 14:09:45 +0600

    JavaScript Calcluter

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
+// document.getElementById('calculator').addEventListener('click', function (event) {
+//     const calculatorBtn = event.target.innerText;
+//     const displayField = document.getElementById('display').innerText;
+
+//     if (calculatorBtn === "C") {
+//         document.getElementById('display').value = "";
+//     }
+//     else if (calculatorBtn === '<') {
+//         document.getElementById('display').value = displayField.slice(0, displayField.length - 1);
+//     }
+//     else if (calculatorBtn === '=') {
+//         document.getElementById('display').value = eval(displayField);
+//     }
+//     else {
+//         document.getElementById('display').value = displayField + calculatorBtn;
+//     }
+// });
+
 document.getElementById('calculator').addEventListener('click', function (event) {
     const calculatorBtn = event.target.innerText;
-    const displayField = document.getElementById('display-Field').innerText;
+    const displayField = document.getElementById('display').value;
 
     if (calculatorBtn === "C") {
-        document.getElementById('display-Field').innerText = "";
+        document.getElementById('display').value = "";
     }
     else if (calculatorBtn === '<') {
-        document.getElementById('display-Field').innerText = displayField.slice(0, displayField.length - 1);
+        document.getElementById('display').value = displayField.slice(0, displayField.length - 1);
     }
     else if (calculatorBtn === '=') {
-        document.getElementById('display-Field').innerText = eval(displayField);
+        document.getElementById('display').value = eval(displayField);
     }
     else {
-        document.getElementById('display-Field').innerText = displayField + calculatorBtn;
+        document.getElementById('display').value = displayField + calculatorBtn;
     }
-});
+})
